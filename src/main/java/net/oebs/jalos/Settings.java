ebef63de5b8d3c3cf2ed87341e7d05f8616e62d9
Oliver Seemann <oseemann@gmail.com>
2014-09-13 12:19:34 +0200

    Add license

--- src/main/java/net/oebs/jalos/Settings.java
+++ src/main/java/net/oebs/jalos/Settings.java
@@ -1,10 +1,28 @@
+/*
+ * Copyright (c) 2014 Oliver Seemann
+ *
+ * This file is part of Jalos.
+ *
+ * Jalos is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jalos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jalos.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.oebs.jalos;
 
-import net.oebs.jalos.errors.SettingsError;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import net.oebs.jalos.errors.SettingsError;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
